3170a4bf7ec49aaf5e870757344a9b9e9c009cef
Heather Kim <haesoo1108@gmail.com>
2019-05-05 23:48:29 +0900

    implemented switching between scrolly and relative

--- d3withflex/d3content.js
+++ d3withflex/d3content.js
@@ -8,3 +8,18 @@ waffle.selectAll('.block')
 .append('div')
 .attr('class', 'block')
 .style('background-color', d => (d < 5 ? '#FE4A49' : '#CCCCCC'));
+
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    document.getElementById('scrolly').style.position = "relative";
+    document.getElementById('scrolly').style.top = "750px";
+  } else {
+    document.getElementById('scrolly').style.position = "sticky";
+    document.getElementById('scrolly').style.top = "300px";
+  }
+}
